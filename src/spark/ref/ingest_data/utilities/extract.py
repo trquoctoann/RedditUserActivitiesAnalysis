db5200dc8133b8dc8e9de98a1a7ebe7a76b87f26
trquoctoann <quoctoan09072002@gmail.com>
2023-05-23 23:58:55 +0700

    delete file

--- src/spark/ref/ingest_data/utilities/extract.py
+++
@@ -1,8 +0,0 @@
-import os 
-import datetime
-
-def get_today() :
-    date_format = '%Y%m%d'
-    today = datetime.date.today()
-    formatted_date = today.strftime(date_format)
-    return str(formatted_date)
